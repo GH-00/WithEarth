588cb34add95cd5e1860eeaeabb81cd166f031e3
TwoMal2 <x__x73@naver.com>
2021-08-05 23:21:26 +0900

    Modify jjim Activity

--- app/src/main/java/com/example/withearth/JJimActivity.java
+++ app/src/main/java/com/example/withearth/JJimActivity.java
@@ -1,17 +1,23 @@
 package com.example.withearth;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class JJimActivity extends Fragment {
+import java.util.ArrayList;
 
+public class JJimActivity extends Fragment {
     private View view;
+    private ListView jjim_product;
+    ArrayList<JjimProduct> products;
 
     @Nullable
     @org.jetbrains.annotations.Nullable
@@ -19,6 +25,52 @@ public class JJimActivity extends Fragment {
     public View onCreateView(@NonNull @org.jetbrains.annotations.NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.activity_jjim, container, false);
 
+        jjim_product = (ListView) view.findViewById(R.id.jjim_product);
+        JjimAdapter adapter = new JjimAdapter();
+        Resources res = getResources();
+
+        products = new ArrayList<JjimProduct>();
+        products.add(new JjimProduct(res.getDrawable(R.drawable.ic_environment) ,"7,000","나뭇잎"));
+
+
+        jjim_product.setAdapter(adapter);
+
         return view;
     }
+
+
+    public class JjimAdapter extends BaseAdapter {
+
+        public void addItem(JjimProduct product){
+            products.add(product);
+        }
+
+        @Override
+        public int getCount() {
+            return products.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return products.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            JjimProductView productView = new JjimProductView(getContext());
+            JjimProduct product = products.get(position);
+
+            productView.setImage(product.getProductImage());
+            productView.setName(product.getName());
+            productView.setPrice(product.getPrice());
+
+            return view;
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/withearth/JjimProduct.java
@@ -0,0 +1,51 @@
+package com.example.withearth;
+
+import  android.graphics.drawable.Drawable;
+
+public class JjimProduct {
+
+    private Drawable productImage;
+    private String price;
+    private String name;
+    private boolean selectable = true;
+
+    public JjimProduct(Drawable product_image, String price, String name){
+        this.productImage = product_image;
+        this.price = price;
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSelectable(boolean selectable){
+        this.selectable = selectable;
+    }
+
+    public Drawable getProductImage(){
+        return productImage;
+    }
+
+    public boolean isSelectable(){
+        return selectable;
+    }
+
+    @Override
+    public String toString() {
+        return "상품: " + name + '\n' +
+                "가격: " + price + '\n';
+    }
+}
