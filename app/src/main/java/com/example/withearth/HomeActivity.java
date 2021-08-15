79852c15f25b999dcc8c3ac47fe0c0d79121d565
GH-00 <ghkim7319@gmail.com>
2021-08-15 14:58:17 +0900

    point info

--- app/src/main/java/com/example/withearth/HomeActivity.java
+++ app/src/main/java/com/example/withearth/HomeActivity.java
@@ -1,23 +1,43 @@
 package com.example.withearth;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import static com.example.withearth.R.color.design_default_color_primary_dark;
 
 public class HomeActivity extends Fragment {
-    
+
     private View view;
+    private Button btn;
 
     @Nullable
     @org.jetbrains.annotations.Nullable
     @Override
-    public View onCreateView(@NonNull @org.jetbrains.annotations.NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull @org.jetbrains.annotations.NotNull LayoutInflater inflater,
+                             @Nullable @org.jetbrains.annotations.Nullable ViewGroup container,
+                             @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+
         view = inflater.inflate(R.layout.activity_home, container, false);
+
+        btn = (Button) view.findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), HomePointInfo.class);
+                startActivity(intent);
+            }
+        });
 
         return view;
     }
---
+++ app/src/main/java/com/example/withearth/HomePointInfo.java
@@ -0,0 +1,14 @@
+package com.example.withearth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomePointInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_point_info);
+    }
+}
