255cf24f66877b6024da822bbc283a95585e8152
TwoMal2 <x__x73@naver.com>
2021-08-18 15:20:03 +0900

    Add RegisterAgreementActivity

---
+++ app/src/main/java/com/example/withearth/RegisterAgreementActivity.java
@@ -0,0 +1,49 @@
+package com.example.withearth;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class RegisterAgreementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_agreement);
+
+
+        //커스텀 툴바 생성
+        Toolbar base_toolbar = findViewById(R.id.base_toolbar);
+        setSupportActionBar(base_toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+
+    }
+
+
+
+
+    //툴바 뒤로가기 버튼 클릭 시
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+
+}
